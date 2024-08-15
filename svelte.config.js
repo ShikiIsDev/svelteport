26972196e84fa7b29b035f83a037cb86d4c484e9
ShikiIsDev <104403831+ShikiIsDev@users.noreply.github.com>
2024-08-15 08:29:18 +0800

    initial

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
-import adapterStatic from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 
 const dev = process.env.NODE_ENV === 'development';
 
 export default {
     kit: {
         paths: {
-            base: dev ? '' : '/svelteport',
+            base: dev ? '' : '/svelteport',  // Adjust the base path as needed
         },
-        adapter: adapterStatic(),
+        adapter: adapter({
+            // default options, customize as needed
+            pages: 'build',
+            assets: 'build',
+            fallback: null
+        }),
     }
 };
